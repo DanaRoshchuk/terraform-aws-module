510e92ba0601629d61d8482c80271141fa4847a9
DanaRoshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2023-04-12 10:11:08 +0300

    Update main.tf

--- fol1/main.tf
+++ fol1/main.tf
@@ -1,3 +1,28 @@
-    module "v6" {
- source = "./fol3"
-} 
+
+        variable "run_id" {
+  default = "run_id_2"
+  sensitive = true
+}
+
+variable "sleep_time" {
+  default = 15
+}
+
+variable "dynatrace-operator_enabled" {
+  description = "dynatrace-operator_enabled" 
+
+type = bool 
+  default = true
+  
+}
+
+
+resource "null_resource" "resource3" {
+  count = 5
+  provisioner "local-exec" {
+    command = "echo $ENV"
+    environment = {
+      ENV = "Hello World!"
+    }
+ }
+}
