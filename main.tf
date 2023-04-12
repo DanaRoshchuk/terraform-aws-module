971bcd480a4b0c4a169890448083871aa31d3400
DanaRoshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2023-04-12 10:11:23 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,3 @@
-variable "run_id" {
-  default = "run_id_1"
-  #sensitive = true
-}
-variable "sleep_time" {
-  default = 3
-}
-variable "dynatrace-operator_enabled" {
-  description = "dynatrace-operator_enabled" 
-sensitive = true
-type = bool 
-  default = true
-  
-}
-  variable "prefix" {
-    default = "Mrs"
-    type = string
-  }
-resource "random_integer" "ran" {
-  count = 1
-  min = 31
-  max = 200
-  keepers = {
-    run_id = var.run_id
-  }
-}
+    module "v6" {
+ source = "./fol3"
+} 
